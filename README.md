4dceadb1fe4bd3872f3d8be31ee3d56e3662ed58
Serika <fsphb@icloud.com>
2024-09-12 23:29:58 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Serika's Swift Package Collections 
+
+A few swift packages collections that I commonly use 
+
+Each folders here contains a Swift Package Collections, which is the `collection.json` file. Add the url of this file to your swift package collections using the following command 
+
+```sh
+swift package-collection add <url-to-collection.json>
+```
+
---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PWD=$(pwd)
+
+PACKAGES_JSON="packages.json"
+GENERATED_JSON="collection.json"
+
+find "$PWD" -type d | while read -r dir; do
+    if [ -f "$dir/$PACKAGES_JSON" ]; then
+        echo "Processing $dir"
+        cd "$dir" || exit 1
+        package-collection-generate $PACKAGES_JSON $GENERATED_JSON
+        cd "$PWD" || exit 1
+    fi
+done
