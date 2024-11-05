40c4627401e5fdc0718c33d3efb10920992290f6
Serika <fsphb@icloud.com>
2024-11-05 04:13:49 -0800

    add swift-system

--- generate.sh
+++ generate.sh
@@ -9,7 +9,7 @@ find "$PWD" -type d | while read -r dir; do
     if [ -f "$dir/$PACKAGES_JSON" ]; then
         echo "Processing $dir"
         cd "$dir" || exit 1
-        package-collection-generate $PACKAGES_JSON $GENERATED_JSON
+        env package-collection-generate $PACKAGES_JSON $GENERATED_JSON
         cd "$PWD" || exit 1
     fi
 done
